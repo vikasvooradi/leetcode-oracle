84c728853839a09845c503ba5ca479a86d4e3459
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2026-01-26 14:39:09 +0530

    586. Customer Placing the Largest Number of Orders

--- problems/586. Customer Placing the Largest Number of Orders.sql
+++ problems/586. Customer Placing the Largest Number of Orders.sql
@@ -1,5 +1,11 @@
 /* Write your PL/SQL query statement below */
 
+-- Note on Edge Cases:  
+-- Although the problem statement guarantees a single top customer, in practice there could be test cases where multiple customers have the same maximum number of orders. The query should be robust enough to handle ties and return all such customers, not just one.
+
+
+/*
+
 SELECT customer_number
 FROM (
         SELECT customer_number,
@@ -9,3 +15,24 @@ FROM (
         GROUP  BY customer_number
      )
 WHERE  rnk = 1; 
+
+*/
+
+SELECT customer_number
+FROM (
+    SELECT customer_number, COUNT(order_number) AS cnt_orders
+    FROM Orders
+    GROUP BY customer_number
+)
+WHERE cnt_orders = (
+    SELECT MAX(cnt_orders)
+    FROM (
+        SELECT COUNT(order_number) AS cnt_orders
+        FROM Orders
+        GROUP BY customer_number
+    )
+);
+
+
+
+
