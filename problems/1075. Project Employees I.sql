e7657cff18807d715c93b3a6955da784a56c5877
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2026-01-27 14:18:27 +0530

    1075. Project Employees I

---
+++ problems/1075. Project Employees I.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+
+select project_id,round(avg(experience_years),2) as average_years 
+from Project join Employee using (employee_id) group by project_id;
