146f67edf60a448301b3d97727c39f3c99c68031
Vikas Bhaskar Vooradi <vikas.b.vooradi@gmail.com>
2026-02-14 13:54:28 +0530

    1693. Daily Leads and Partners

---
+++ sql-solutions/1693. Daily Leads and Partners.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+
+select 
+    to_char(date_id,'YYYY-MM-DD') as date_id,
+    make_name,
+    count(distinct lead_id) as unique_leads,
+    count(distinct partner_id) as unique_partners from DailySales
+group by date_id,make_name;
